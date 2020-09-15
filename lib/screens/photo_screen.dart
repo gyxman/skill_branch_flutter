f78e9891b73cec2c0e036bbe4d768fbb03986f16
Roman Aleksyutin <gyxman@yandex.ru>
2020-09-16 00:32:27 +0300

    fixed homework

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -98,3 +98,64 @@ class _FullScreenImageState extends State<FullScreenImage> {
             ]));
   }
 }
+
+class Button extends StatelessWidget {
+  Button({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 105,
+      height: 36,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: AppColors.dodgerBlue,
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: Text(
+        this.text,
+        style: TextStyle(color: AppColors.white),
+      ),
+    );
+  }
+}
+
+class Description extends StatelessWidget {
+  Description({Key key, this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Text(text,
+          maxLines: 3, overflow: TextOverflow.ellipsis, style: AppStyles.h3),
+    );
+  }
+}
+
+class Name extends StatelessWidget {
+  Name({this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(this.name, style: AppStyles.h1Black);
+  }
+}
+
+class Nickname extends StatelessWidget {
+  Nickname({this.userName});
+
+  final String userName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('@${this.userName}',
+        style: AppStyles.h5Black.copyWith(color: AppColors.manatee));
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,7 +1,3 @@
-export 'button.dart';
-export 'description.dart';
 export 'like_button.dart';
-export 'name.dart';
-export 'nickname.dart';
 export 'photo.dart';
 export 'user_avatar.dart';
